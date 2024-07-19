98064e6b924d085d88c3b0b668c9498a8c9a9418
pavani <pavani.machineni666@gmail.com>
2024-07-19 15:32:07 +0530

    first commit

--- src/Quote generator.js
+++ src/Quote generator.js
@@ -7,6 +7,7 @@ const App = () => {
 
   const fetchQuote = async () => {
     try {
+      
       const response = await fetch('https://type.fit/api/quotes');
       const quotes = await response.json();
       const randomIndex = Math.floor(Math.random() * quotes.length);
